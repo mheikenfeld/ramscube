4746db2902a128f2b9371ffd017e57193055934b
Max Heikenfeld <maxheikenfeld@web.de>
2018-04-02 05:33:24 +0100

    added load to __init__.py

--- ramscube/__init__.py
+++ ramscube/__init__.py
@@ -1,4 +1,4 @@
-from .ramscube import loadramscube,deriveramscube
+from .ramscube import loadramscube,deriveramscube,load
 
 #, load
 #from ramscube.ramscube import calculate_wrf_LWP_fromcubes,calculate_wrf_IWP_fromcubes,calculate_wrf_IWV_fromcubes,integrate_cube
